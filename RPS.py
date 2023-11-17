1b5f072bee1cf55dd5544a0c8278e0afb3079f90
Mogra12 <86124740+Mogra12@users.noreply.github.com>
2023-11-17 14:58:51 -0300

    Add files via upload

---
+++ RPS.py
@@ -0,0 +1,34 @@
+import random
+from time import sleep
+
+rps = ['rock', 'papers', 'scisors']
+
+interface = """
+Rock
+Papers
+Scissors
+"""
+
+def rps_game():
+    print(interface)
+    user_choice = input("Enter your choice: ").lower()
+    computer_choice = random.choice(rps)
+    print(f"Computer choice: {computer_choice}")
+    sleep(1)
+
+    if user_choice == computer_choice:
+        print("It's a tie!")
+    elif user_choice == 'rock' and computer_choice == 'scisors':
+        print("You won!")
+    elif user_choice == 'rock' and computer_choice == 'papers':
+        print("You lost!")
+    elif user_choice == 'papers' and computer_choice == 'rock':
+        print("You won!")
+    elif user_choice == 'papers' and computer_choice =='scisors':
+        print("You lost!")
+    elif user_choice =='scisors' and computer_choice == 'rock':
+        print("You lost!")
+    elif user_choice =='scisors' and computer_choice == 'papers':
+        print("You won!")
+
+rps_game()
